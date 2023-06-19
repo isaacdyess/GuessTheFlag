4d088a74bd38349a3bf13798e4f95398ea4e613f
Isaac <43579566+isaacdyess@users.noreply.github.com>
2023-06-19 18:55:29 -0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Isaac Dyess on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
